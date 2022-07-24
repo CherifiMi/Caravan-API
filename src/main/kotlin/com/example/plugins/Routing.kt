c2df4b1d9e588617f29ebd539b3eaad7880e7bd6
CherifiMi <cherifi154949@gmail.com>
2022-07-24 04:03:50 +0100

    buyer route

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,16 +1,45 @@
 package com.example.plugins
 
+import com.example.models.Buyer
 import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
+import org.litote.kmongo.coroutine.CoroutineCollection
+import org.litote.kmongo.coroutine.CoroutineDatabase
 
-fun Application.configureRouting() {
+fun Application.configureRouting(database: CoroutineDatabase) {
 
     routing {
         get("/") {
             call.respondText("Hello Mito!")
+        }
+    }
+    //______________caravan api v1
+    routing { route("/v1"){
+
+        buyers(database.getCollection("buyers"))
+
+    }}
+}
+
+fun Route.buyers(collection: CoroutineCollection<Buyer>) {
+    route("/buyers"){
+        get {
+            try{
+                call.respond(collection.find().toList())
+            }catch (e: Exception){
+                call.respondText("ERROR: " + e.toString())
+            }
+
+        }
+        post {
+            call.respondText("post buyers!")
+        }
+        put {
+            call.respondText("put buyers!")
+        }
+        delete {
+            call.respondText("delete buyers!")
         }
     }
 }
