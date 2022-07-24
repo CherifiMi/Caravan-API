ddbbedc9725a6851ab6f9416c9048ca39913a355
CherifiMi <cherifi154949@gmail.com>
2022-07-24 04:02:19 +0100

    added rep entity

---
+++ src/main/kotlin/com/example/models/rep.kt
@@ -0,0 +1,15 @@
+package com.example.models
+
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+
+data class rep (
+    @BsonId
+    val id: String= ObjectId().toString(),
+    val name: String,
+    val phone: String,
+    val autheId: String,
+    val mySellers: List<String>,
+    val myBuyers: List<String>,
+    val isActive: Boolean
+)
