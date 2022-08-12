77f1fc63017842efecfcbf8a218660bd8bc7b834
CherifiMi <cherifi154949@gmail.com>
2022-08-12 07:59:44 +0100

    get buyer by auth key

--- src/main/kotlin/com/example/routes/HandelUsers.kt
+++ src/main/kotlin/com/example/routes/HandelUsers.kt
@@ -29,7 +29,7 @@ fun Route.buyers(
 
         }
         //get by auth key
-        get("/auth") {
+        post("/auth") {
             try {
                 call.parameters
                 val requestBody = call.receive<Id>()
@@ -39,17 +39,7 @@ fun Route.buyers(
             }
 
         }
-        //get by auth key
-        get("/id") {
-            try {
-                call.parameters
-                val requestBody = call.receive<Id>()
-                call.respond(collection.findOne(Buyer::id eq requestBody.id).toString())
-            } catch (e: Exception) {
-                call.respondText("ERROR: " + e.toString())
-            }
 
-        }
         //create new buyer
         post {
             call.parameters
