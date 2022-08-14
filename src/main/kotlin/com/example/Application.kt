f837530f0131610fd226a8ae3ce84e2b6b7471dc
CherifiMi <cherifi154949@gmail.com>
2022-08-14 01:22:44 +0100

    changed key

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -20,7 +20,7 @@ val client = KMongo
 val database = client.getDatabase("caravan_db")
 fun main() {
 
-    Stripe.apiKey = "sk_test_51LWC3uSId0KyzvnEJlzBPdTOOEv4S6kMWzfnVdN9TqGmjjLuUtABPz69cSTrSU3AU3RYC3PvhEaRiagOUJxz1ESj00FB5rlJmo"
+    Stripe.apiKey = "sk_test_51LWUO5SACQklfQRYVIkYexK54QtlGbicWiwlaf8qv9aG9o4Yh1S4MQ0XVtOmkt5mAovIu5pKxbuj3ZedzxmKFyGz009gXuiRq2"
 
     embeddedServer(Netty,  port = System.getenv("PORT").toInt()) {
         configureHTTP()
