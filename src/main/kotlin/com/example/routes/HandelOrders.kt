eaa8105f141508cad8833ede2ec1bc1d8c50fe8b
CherifiMi <cherifi154949@gmail.com>
2022-08-23 10:56:35 +0100

    reduce product inv when making new order fixed

--- src/main/kotlin/com/example/routes/HandelOrders.kt
+++ src/main/kotlin/com/example/routes/HandelOrders.kt
@@ -8,6 +8,7 @@ import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.litote.kmongo.coroutine.CoroutineCollection
+import org.litote.kmongo.coroutine.replaceOne
 import org.litote.kmongo.eq
 
 fun Route.orders(collection: CoroutineCollection<Order>, collectionP: CoroutineCollection<Product>) {
@@ -25,14 +26,24 @@ fun Route.orders(collection: CoroutineCollection<Order>, collectionP: CoroutineC
             call.parameters
             val requestBody = call.receive<Order>()
 
+            //oldTest.copy(age = oldTest.age - 2, name = "yooo")
+            //collectionP.replaceOne(Product::id eq requestBody.productId, newProduct).wasAcknowledged()
+            //val thisProduct: Product = collectionP.findOne(Product::id eq requestBody.productId)!!
+            //val newProduct = thisProduct?.copy(amountInInv = thisProduct.amountInInv.minus(requestBody.amount))
 
-            val thisProduct = collectionP.findOne(Product::id eq requestBody.productId)
+            // get the current product
+            val currentProduct: Product =  collectionP.findOne(Product::id eq requestBody.productId)!!
 
-            val newProduct = thisProduct!!.copy(amountInInv = thisProduct.amountInInv.minus(requestBody.amount))
+            // make a new product from the old one
+            val changedProduct = currentProduct.copy(amountInInv = currentProduct.amountInInv - requestBody.amount)
+
+            //replace the old one with the new one
+
 
             val isSuccess =
-                collection.insertOne(requestBody).wasAcknowledged()
-                        && collectionP.replaceOne(newProduct.id, newProduct).wasAcknowledged()
+                collection.insertOne(requestBody).wasAcknowledged() &&
+                        collectionP.replaceOne(changedProduct).wasAcknowledged()
+
             call.respond(isSuccess)
         }
 
