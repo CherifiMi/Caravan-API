0b9d72783983ba4886837f82018f688ca084951e
CherifiMi <cherifi154949@gmail.com>
2022-08-24 13:27:43 +0100

    new db

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -5,8 +5,6 @@ import com.example.plugins.configureRouting
 import com.example.plugins.configureSerialization
 import com.example.plugins.configureSockets
 import com.stripe.Stripe
-import com.stripe.model.Account
-import com.stripe.param.AccountCreateParams
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import org.litote.kmongo.coroutine.coroutine
@@ -15,7 +13,8 @@ import org.litote.kmongo.reactivestreams.KMongo
 
 val client = KMongo
     .createClient(
-        "mongodb+srv://mito:cherifi2003@mitocluster.bpzkl.mongodb.net/caravan_db?retryWrites=true&w=majority"
+        //"mongodb+srv://mito:cherifi2003@mitocluster.bpzkl.mongodb.net/caravan_db?retryWrites=true&w=majority"
+        "mongodb+srv://mito:cherifi2003@caravancluster.ztgh797.mongodb.net/caravan_db?retryWrites=true&w=majority"
     ).coroutine
 val database = client.getDatabase("caravan_db")
 fun main() {
